2ebb7a377a300294f1958a365ba7a506f9b5e9cd
阿樊提 <fanjiahe2000@163.com>
2017-05-06 20:24:08 +0800

    Update gitserver.go

--- cmd/gitserver/gitserver.go
+++ cmd/gitserver/gitserver.go
@@ -1,5 +1,5 @@
 package main
-
+//使用main包的main函数作为入口，启动程序的层层执行
 import (
 	"math/rand"
 	"os"
@@ -13,7 +13,7 @@ import (
 	"github.com/openshift/origin/pkg/cmd/util/serviceability"
 
 	// install all APIs
-	_ "github.com/openshift/origin/pkg/api/install"
+	_ "github.com/openshift/origin/pkg/api/install" //安装包用来实现安装
 	_ "k8s.io/kubernetes/pkg/api/install"
 	_ "k8s.io/kubernetes/pkg/apis/extensions/install"
 )
@@ -30,8 +30,8 @@ func main() {
 	}
 
 	basename := filepath.Base(os.Args[0])
-	command := gitserver.CommandFor(basename)
-	if err := command.Execute(); err != nil {
-		os.Exit(1)
+	command := gitserver.CommandFor(basename)//包的位置在pkg／cmd目录中；
+	if err := command.Execute(); err != nil {//执行execute函数
+		os.Exit(1)//退出程序，给出退出状态
 	}
 }
